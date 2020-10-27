2c04c35bfbbcf33e4627ccdad5c918d27bbd873f
ace <ace@ezotop.ru>
2020-10-28 00:12:22 +0300

    add extras

---
+++ extras/disable.sh
@@ -0,0 +1,6 @@
+systemctl stop nginx.service
+systemctl disable nginx.service
+systemctl stop moonraker.service
+systemctl disable moonraker.service
+systemctl stop klipper.service
+systemctl disable klipper.service
---
+++ extras/enable.sh
@@ -0,0 +1,6 @@
+systemctl enable klipper.service
+systemctl enable moonraker.service
+systemctl enable nginx.service
+systemctl start klipper.service
+systemctl start moonraker.service
+systemctl start nginx.service
